60613058f863f651c97976ff3851d9805dc4a2e6
mintaka33 <mintaka33@outlook.com>
2018-07-30 15:49:19 +0800

    add capture desktop class

---
+++ capture/capture/capture.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "capture.h"
+
+
+CaptureDesktop::CaptureDesktop()
+{
+}
+
+CaptureDesktop::~CaptureDesktop()
+{
+}
+
+
---
+++ capture/capture/capture.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "stdafx.h"
+
+class CaptureDesktop
+{
+public:
+    CaptureDesktop();
+    ~CaptureDesktop();
+
+private:
+
+};
+
+
