952c5955462155e7c5739d5191ae8ab238b06126
mintaka33 <mintaka33@outlook.com>
2018-07-30 16:09:15 +0800

    add basic interface of capture class

--- capture/capture/capture.cpp
+++ capture/capture/capture.cpp
@@ -10,4 +10,18 @@ CaptureDesktop::~CaptureDesktop()
 {
 }
 
+INT CaptureDesktop::initialize()
+{
+    return 0;
+}
+
+INT CaptureDesktop::getFrame(FrameProp* prop)
+{
+    return 0;
+}
+
+void CaptureDesktop::destroy()
+{
+
+}
 
--- capture/capture/capture.h
+++ capture/capture/capture.h
@@ -5,14 +5,40 @@
 #include <d3d11.h>
 #include <dxgi1_2.h>
 
+struct FrameProp
+{
+    BYTE* pData;
+    INT pitch;
+    INT height;
+    INT size;
+};
+
 class CaptureDesktop
 {
 public:
     CaptureDesktop();
     ~CaptureDesktop();
 
+    INT initialize();
+    INT getFrame(FrameProp* prop);
+    void destroy();
+
 private:
+    ID3D11Device* pD3D11Device_ = nullptr;
+    ID3D11DeviceContext* pDeviceContext_ = nullptr;
+    IDXGIDevice* pDxgiDevice_ = nullptr;
+    IDXGIAdapter* pDxgiAdapter_ = nullptr;
 
+    IDXGIOutput* pDxgiOutput_ = nullptr;
+    IDXGIOutput1* pDxgiOutput1_ = nullptr;
+    IDXGIOutputDuplication* pOutputDupl_ = nullptr;
+
+    IDXGIResource* pDesktopResource_ = nullptr;
+    ID3D11Texture2D* pDesktopSrc_ = nullptr;
+    ID3D11Texture2D* pDesktopDst_ = nullptr;
+
+    BYTE* pData_ = nullptr;
+    INT pitch_ = 0;
+    INT height_ = 0;
 };
-
 
