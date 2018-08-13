e911aefe48ab276704c2c2a3904068d1a30e2b84
mintaka33 <mintaka33@outlook.com>
2018-08-13 09:52:33 +0800

    add basic interface for overlay class

--- overlay/overlay/overlay.cpp
+++ overlay/overlay/overlay.cpp
@@ -2,11 +2,42 @@
 #include "stdafx.h"
 #include "overlay.h"
 
-YuvOverlay::YuvOverlay()
+YuvOverlay::YuvOverlay(std::string fileName, int w, int h, std::string fmt):
+    fileName_(fileName), width_(w), height_(h), format_(fmt)
 {
 }
 
 YuvOverlay::~YuvOverlay()
 {
+}
+
+int YuvOverlay::initialize()
+{
+    return 0;
+}
+
+int YuvOverlay::overlayFrame(int frameIndex, int value)
+{
+    return 0;
+}
+
+int YuvOverlay::destroy()
+{
+    return 0;
+}
+
+int YuvOverlay::readFrame(int frameIndex)
+{
+    return 0;
+}
+
+int YuvOverlay::overlay()
+{
+    return 0;
+}
+
+int YuvOverlay::writeFrame()
+{
+    return 0;
 }
 
--- overlay/overlay/overlay.h
+++ overlay/overlay/overlay.h
@@ -1,15 +1,30 @@
 #pragma once
 
 #include "stdafx.h"
+#include <string>
 
 class YuvOverlay
 {
 public:
-    YuvOverlay();
+    YuvOverlay(std::string fileName, int w, int h, std::string fmt);
     ~YuvOverlay();
 
+    int initialize();
+    int overlayFrame(int frameIndex, int value);
+    int destroy();
+
 private:
-    int32_t width;
-    int32_t height;
-    int32_t frameNum;
+    int readFrame(int frameIndex);
+    int overlay();
+    int writeFrame();
+
+private:
+    int width_ = 0;
+    int height_ = 0;
+    std::string format_ = "";
+    std::string fileName_ = "";
+    int frameNum_ = 0;
+    char* pY_ = nullptr;
+    char* pU_ = nullptr;
+    char* pV_ = nullptr;
 };
